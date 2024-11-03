d6404ca59384a39260efc42e6a442be45aaf54b4
Wendler Juan Jose <juanjoowendler@gmail.com>
2024-11-02 21:52:19 -0300

    se controla que no ingrese a zona restringida, falta controlar que no se aleje de la agencia

--- microservicio-vehiculos/src/main/java/com/agencia/microservicio_vehiculos/controller/VehiculoController.java
+++ microservicio-vehiculos/src/main/java/com/agencia/microservicio_vehiculos/controller/VehiculoController.java
@@ -3,6 +3,7 @@ package com.agencia.microservicio_vehiculos.controller;
 import com.agencia.microservicio_vehiculos.entities.ConfiguracionResponse;
 import com.agencia.microservicio_vehiculos.entities.Posicion;
 import com.agencia.microservicio_vehiculos.entities.Vehiculo;
+import com.agencia.microservicio_vehiculos.entities.ZonaRestringida;
 import com.agencia.microservicio_vehiculos.services.ConfiguracionService;
 import com.agencia.microservicio_vehiculos.services.PruebaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/vehiculos")
@@ -42,8 +44,26 @@ public class VehiculoController {
         LocalDateTime fechaHora = LocalDateTime.parse(posicion.getFechaHora());
 
         if (!pruebaService.isVehiculoInPruebasAndCumpleLimites(vehiculo, fechaHora)) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("El vehiculo se encuentra en una prueba.");
-            // luego controlaria si esta dentro de los limites..
+            double latitud = posicion.getLatitud();
+            double longitud = posicion.getLongitud();
+
+            ConfiguracionResponse configuracionAPI = configuracionService.obtenerConfiguracion();
+            List<ZonaRestringida> zonasRestringidas = configuracionAPI.getZonasRestringidas();
+            System.out.println(zonasRestringidas);
+            // Iterar sobre las zonas restringidas
+            for (ZonaRestringida zona : zonasRestringidas) {
+                double latNoroeste = zona.getNoroeste().getLat();
+                double lonNoroeste = zona.getNoroeste().getLon();
+                double latSureste = zona.getSureste().getLat();
+                double lonSureste = zona.getSureste().getLon();
+
+                // Comprobar si la posición está dentro de la zona restringida
+                if (latitud <= latNoroeste && latitud >= latSureste && longitud >= lonNoroeste && longitud <= lonSureste) {
+                    return ResponseEntity.status(HttpStatus.CONFLICT).body("El vehiculo se encuentra en una prueba y su posicion esta dentro de una zona restringida.");
+                    // enviar mensaje...
+                }
+            }
+            return ResponseEntity.status(HttpStatus.OK).body("El vehiculo se encuentra en una prueba y su posicion no esta dentro de una zona restringida.");
         }
 
         return ResponseEntity.status(HttpStatus.OK).body("El vehiculo no se encuentra en una prueba.");
